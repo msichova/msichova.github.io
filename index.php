667e251f151c21831e28cd4a066d6d0b491c488e
Marina Sichova <msichova@outlook.com>
2024-01-23 23:09:07 +0800

    Added files
    Added files index.php and page-style\style-page.css

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE.html>
+<html lang='en'>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <meta name='description' content='portfolio, web, development, software development'>
+        <meta name='author' content='Marina Sichova, msichova'>
+        <link href='./page-styles/style-page.css' rel='stylesheet'>
+    </head>
+    <body class='mainPage'>
+        <p >
+        <h1>Marina Sichova</h1>
+        <h3>Junior Software Developer</h3>
+    </p>
+    </body>
+</html>
