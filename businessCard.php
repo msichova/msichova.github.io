b53f8eb68d5d5d9f5b09a904727193ab59caa91b
Marina Sichova <35139158+msichova@users.noreply.github.com>
2024-01-24 18:33:37 +0800

    Update businessCard.php

--- businessCard.php
+++ businessCard.php
@@ -3,7 +3,7 @@
 <link href='./page-styles/style-page.css' rel='stylesheet'>
     <header>
         <div class='businessCardPart'>
-            <h2>Marina Sichova<br>Junior Back-End Software Developer<\h2>
+            <h2>Marina Sichova<br>Junior Back-End Software Developer</h2>
             <h4 class='skillsBusinessCard'>C#, JavaScript, PHP, Java <br>ASP.NET CORE: API, Web App<br>SQL, mySQL,<br>noSQL(MongoDB Atlas and Azure Cashe for Redis) 
             </h4>
         </div>
